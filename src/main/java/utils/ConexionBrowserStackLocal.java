4e16336fb0141bd688d65d36c271e907c8a71b2f
Dnarva5 <DNARVA5@bancodebogota.com.co>
2024-11-29 09:56:31 -0500

    :construcion: PFM

---
+++ src/main/java/utils/ConexionBrowserStackLocal.java
@@ -0,0 +1,30 @@
+package utils;
+
+import com.browserstack.local.Local;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class ConexionBrowserStackLocal {
+    static EnvariomentValues env = new EnvariomentValues();
+    private ConexionBrowserStackLocal() {
+    }
+    static Local bsLocal = new Local();
+    static HashMap<String, String> bsLocalArgs = new HashMap<>();
+
+    public static void browserStackLocal()  {
+        bsLocalArgs.put("key", env.getenv("BROWSERSTACK_ACCESS_KEY"));
+        bsLocalArgs.put("geoLocation", "CO");
+        bsLocalArgs.put("localIdentifier", UUID.randomUUID().toString());
+        bsLocalArgs.put("proxyHost", "localhost");
+        bsLocalArgs.put("proxyPort", "8888");
+
+    }
+
+    public static void browserStackLocalfinish() throws Exception {
+        bsLocal.stop();
+    }
+
+
+}
+
--- src/test/java/stepdefinitions/hooks/Hooks.java
+++ src/test/java/stepdefinitions/hooks/Hooks.java
@@ -1,11 +1,18 @@
 package stepdefinitions.hooks;
 
+import io.cucumber.java.After;
+import io.cucumber.java.AfterAll;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import net.serenitybdd.screenplay.actors.OnStage;
 
 import net.thucydides.core.util.EnvironmentVariables;
+import org.openqa.selenium.JavascriptExecutor;
 import stepdefinitions.actors.EnvironmentCast;
+import utils.BrowserstackLogs;
+import utils.ConexionBrowserStackLocal;
+
+import static net.serenitybdd.core.Serenity.getDriver;
 
 public class Hooks {
 
@@ -15,6 +22,22 @@ public class Hooks {
     public void setUp(Scenario scenario) {
         OnStage.setTheStage(new EnvironmentCast(environmentVariables));
 
+    }
+    @After
+    public void checkTestCase(Scenario scenario) {
+        JavascriptExecutor jse = (JavascriptExecutor) getDriver();
+        if (scenario.isFailed()) {
+            jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\","
+                    + " \"arguments\": {\"status\": \"failed\", \"reason\": \"TestFailed\"}}");
+        } else {
+            jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", "
+                    + "\"arguments\": {\"status\": \"passed\", \"reason\": \"TestPassed\"}}");
+        }
+        BrowserstackLogs.informationBrowserstackLogs(getDriver());
+    }
+    @AfterAll
+    public static void stopTunnel() throws Exception {
+        ConexionBrowserStackLocal.browserStackLocalfinish();
     }
 }
 
