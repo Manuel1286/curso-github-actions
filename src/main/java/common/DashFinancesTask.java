477c42b6162a3d3078e76d47c026f0e913b8a420
Dnarva5 <DNARVA5@bancodebogota.com.co>
2024-12-11 14:38:36 -0500

    :recycle: Linter

--- src/main/java/common/DashFinancesTask.java
+++ src/main/java/common/DashFinancesTask.java
@@ -1,20 +1,16 @@
 package common;
-
 import static constants.ConstantsPfm.TEXT_FINANCES;
 import static net.serenitybdd.screenplay.Tasks.instrumented;
 import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
+import static net.serenitybdd.screenplay.questions.WebElementQuestion.stateOf;
 import static ui.pfm.FinancesDemo.TXT_WELCOME_FINANCE;
-import static ui.pfm.FinancesDemo.TXT_QUERY_OPTIONS;
-import static ui.pfm.FinancesDemo.BUTTON_CONTINUE_FINANCE;
-import static ui.pfm.FinancesDemo.BUTTON_DEMO_CLOSE;
-import static ui.pfm.FinancesDemo.LBL_FINANCES;
-
 
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
-import net.serenitybdd.screenplay.waits.WaitUntil;
+import net.serenitybdd.screenplay.waits.Wait;
 import questions.CommonQuestions;
+import ui.pfm.FinancesDemo;
 
 
 public class DashFinancesTask implements Task {
@@ -26,41 +22,21 @@ public class DashFinancesTask implements Task {
   public <T extends Actor> void performAs(T actor) {
     try {
       Thread.sleep(5000);
+      if (TXT_WELCOME_FINANCE.resolveFor(actor).isVisible()) {
+        actor.attemptsTo(
+                Click.on(FinancesDemo.BUTTON_CONTINUE_FINANCE),
+                Wait.until(stateOf(FinancesDemo.TXT_QUERY_OPTIONS), isVisible()).forNoMoreThan(25).seconds(),
+                Click.on(FinancesDemo.BUTTON_DEMO_CLOSE)
+        );
+      } else if (FinancesDemo.TXT_QUERY_OPTIONS.resolveFor(actor).isVisible()) {
+        actor.attemptsTo(
+                Click.on(FinancesDemo.BUTTON_DEMO_CLOSE)
+        );
+      } else {
+        CommonQuestions.textEquals(FinancesDemo.LBL_FINANCES, TEXT_FINANCES);
+      }
     } catch (InterruptedException e) {
       Thread.currentThread().interrupt();
-      throw new RuntimeException("El hilo fue interrumpido durante la espera.", e);
-    }
-    if (isWelcomeFinanceVisible(actor)) {
-        handleWelcomeFinance(actor);
-      } else if (isQueryOptionsVisible(actor)) {
-        closeDemo(actor);
-      } else {
-        validateFinancesLabel(actor);
-      }
-    }
-
-    private <T extends Actor> boolean isWelcomeFinanceVisible(T actor) {
-      return TXT_WELCOME_FINANCE.resolveFor(actor).isVisible();
-    }
-
-    private <T extends Actor> boolean isQueryOptionsVisible(T actor) {
-      return TXT_QUERY_OPTIONS.resolveFor(actor).isVisible();
-    }
-
-    private <T extends Actor> void handleWelcomeFinance(T actor) {
-      actor.attemptsTo(
-              Click.on(BUTTON_CONTINUE_FINANCE),
-              WaitUntil.the(TXT_QUERY_OPTIONS, isVisible()).forNoMoreThan(25).seconds(),
-              Click.on(BUTTON_DEMO_CLOSE)
-      );
-    }
-
-    private <T extends Actor> void closeDemo(T actor) {
-
-    actor.attemptsTo(Click.on(BUTTON_DEMO_CLOSE));
-    }
-
-    private <T extends Actor> void validateFinancesLabel() {
-      CommonQuestions.textEquals(LBL_FINANCES, TEXT_FINANCES);
     }
   }
+}
