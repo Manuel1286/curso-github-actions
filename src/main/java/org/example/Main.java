64c89e6f034cb4be0c51bb966e1f9ee3dfaa9003
Manuel <manuel.1286@hotmail.com>
2026-02-17 14:16:33 -0500

    Ajuste texto

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo Manuel soy el mejor BDB en git");
+        System.out.println("Hola Mundo Manuel soy el mejor BDB en git Manuel");
     }
 
 }
