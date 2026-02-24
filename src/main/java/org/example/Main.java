e1987a3ab0ae2b26c98fe86ab1d1145b2faf9939
Manuel <manuel.1286@hotmail.com>
2026-02-24 13:40:59 -0500

    ajuste gradle

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,1 @@
-package org.example;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hola Mundo Manuel soy el mejor BDB en git Manuel");
-    }
-
-
-
-}
+package org.example;public class Main {    public static void main(String[] args) {        System.out.println("Hola Mundo Manuel soy el mejor BDB en git Gato");    }}
